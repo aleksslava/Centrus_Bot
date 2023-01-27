ae84f30b335d4522383579fc295be7fc2449190a
Vyacheslav_Alekseev <solomonslava1991@gmail.com>
2023-01-28 00:29:12 +0400

    Изменены пути в settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 import os.path
 import pathlib
+from pathlib import Path
 
 # token = "5873053286:AAGFQvZm2o1pHSWlHQ2Q_IJ8F_l1jXfJDTM"
 with open('TOKEN.txt', 'r') as file:
@@ -7,11 +8,12 @@ with open('TOKEN.txt', 'r') as file:
 
 
 class Paths:
-    path_to_database = pathlib.Path('database').absolute().__str__() + r'\data_items.sqlite'
-    path_to_vibors = r'D:\Python_code\Centrus_Bot\vibors_photo'
-    path_to_feldhaus_trot = r'D:\Python_code\Centrus_Bot\feldhaus_trot'
-    path_to_kamelot = r'D:\Python_code\Centrus_Bot\fasade'
-    path_to_portland = r'D:\Python_code\Centrus_Bot\fasade2'
-    path_to_kirpich = r'D:\Python_code\Centrus_Bot\kirpich'
-    path_to_peldano = r'D:\Python_code\Centrus_Bot\peldano'
-    path_to_klinker = r'D:\Python_code\Centrus_Bot\klinker'
+    path_to_database = Path('database\data_items.sqlite')
+    path_to_vibors = Path('vibors_photo')
+    path_to_feldhaus_trot = Path('feldhaus_trot')
+    path_to_kamelot = Path('fasade')
+    path_to_portland = Path('fasade2')
+    path_to_kirpich = Path('kirpich')
+    path_to_peldano = Path('peldano')
+    path_to_klinker = Path('klinker')
+
